19c7d10762671ad3808ed05cd497611529816475
ak38934 <ak38934@ubt-uni.net>
2019-02-02 13:42:57 +0100

    Initiating the project

---
+++ ubtnews/ubtnews/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ubtnews.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

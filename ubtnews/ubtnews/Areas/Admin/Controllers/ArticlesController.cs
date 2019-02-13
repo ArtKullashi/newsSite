9400bbfda88baa035c87f2c54fcfffad28b9c5de
ak38934 <ak38934@ubt-uni.net>
2019-02-13 21:54:10 +0100

    Added admin area and user roles

---
+++ ubtnews/ubtnews/Areas/Admin/Controllers/ArticlesController.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ubtnews.Areas.Admin.Models.Articles;
+using ubtnews.Data;
+using ubtnews.Models;
+using System.IO;
+
+
+namespace ubtnews.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class ArticlesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public string CurrentUserId
+        {
+            get
+            {
+                return User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            }
+        }
+
+        public ArticlesController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Admin/Articles
+        public async Task<IActionResult> Index()
+        {
+            if (User.IsInRole("Administrator"))
+            {
+                var articles = await _context.Articles.ToListAsync();
+                return View(articles);
+            }
+            else
+            {
+                var articles = await _context.Articles
+                                .Where(a => a.Permissions.Exists(au => au.UserId == CurrentUserId))
+                                .ToListAsync();
+
+                return View(articles);
+            }
+        }
+
+        // GET: Admin/Articles/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            if (!await HasAccessToArticle(id.Value))
+            {
+                return View("Error");
+            }
+
+            var article = await _context.Articles
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            return View(article);
+        }
+
+        // GET: Admin/Articles/Create
+        public IActionResult Create()
+        {
+            ViewData["Categories"] = new SelectList(_context.Categories, "Id", "Name");
+            return View();
+        }
+
+        // POST: Admin/Articles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(article);
+
+                _context.Permissions.Add(new Permission
+                {
+                    UserId = CurrentUserId,
+                    Article = article
+                });
+
+                
+
+                _context.Add(article);
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["Categories"] = new SelectList(_context.Categories, "Id", "Name");
+            return View(article);
+        }
+
+        // GET: Admin/Articles/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            if (!await HasAccessToArticle(id.Value))
+            {
+                return View("Error");
+            }
+
+            var article = await _context.Articles.FindAsync(id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["Categories"] = new SelectList(_context.Categories, "Id", "Name");
+            return View(article);
+        }
+
+        // POST: Admin/Articles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Article article, IFormFile imageFile)
+        {
+            if (id != article.Id)
+            {
+                return NotFound();
+            }
+
+            if (!await HasAccessToArticle(id))
+            {
+                return View("Error");
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (imageFile != null)
+                    {
+                        var fileName = Path.GetRandomFileName() + Path.GetExtension(imageFile.FileName);
+                        var fileDirectory = "wwwroot/images";
+
+                        if (!Directory.Exists(fileDirectory))
+                        {
+                            Directory.CreateDirectory(fileDirectory);
+                        }
+
+                        var filePath = fileDirectory + "/" + fileName;
+
+                        // Copy file to path...
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await imageFile.CopyToAsync(stream);
+                        }
+
+                        article.Img = fileName;
+                    }
+                    else
+                    {
+
+                    }
+                    _context.Update(article);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArticleExists(article.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["Categories"] = new SelectList(_context.Categories, "Id", "Name");
+            return View(article);
+        }
+
+        // GET: Admin/Articles/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            if (!await HasAccessToArticle(id.Value))
+            {
+                return View("Error");
+            }
+
+            var article = await _context.Articles
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            return View(article);
+        }
+
+        // POST: Admin/Articles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (!await HasAccessToArticle(id))
+            {
+                return View("Error");
+            }
+
+            var article = await _context.Articles.FindAsync(id);
+            _context.Articles.Remove(article);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> ManageUsers(int id)
+        {
+            var article = await _context.Articles
+                                .Include(a => a.Permissions)
+                                .ThenInclude(au => au.User)
+                                .SingleOrDefaultAsync(a => a.Id == id);
+
+            var allUsers = await _userManager.GetUsersInRoleAsync("NormalUser");
+
+            var vm = new ManageUsersViewModel();
+            vm.Id = id;
+            vm.AssignedUsers = article.Permissions.Select(au => au.User);
+            vm.AvailableUsers = allUsers.Where(u => !vm.AssignedUsers.Any(x => x.Id == u.Id));
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> AddUserToArticle(int id, string userId)
+        {
+            _context.Permissions.Add(new Permission
+            {
+                ArticleId = id,
+                UserId = userId
+            });
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(ManageUsers), new { id = id });
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> RemoveUserFromArticle(int id, string userId)
+        {
+            var au = await _context.Permissions.SingleOrDefaultAsync(a => a.UserId == userId && a.ArticleId == id);
+
+            if (au != null)
+            {
+                _context.Permissions.Remove(au);
+
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                return NotFound();
+            }
+
+            return RedirectToAction(nameof(ManageUsers), new { id = id });
+        }
+
+        private async Task<bool> HasAccessToArticle(int articleId)
+        {
+            if (User.IsInRole("Administrator"))
+                return true;
+
+            var article = await _context.Articles
+                                .Where(a => a.Id == articleId)
+                                .Where(a => a.Permissions.Exists(au => au.UserId == CurrentUserId))
+                                .SingleOrDefaultAsync();
+
+            return article != null;
+        }
+
+        private bool ArticleExists(int id)
+        {
+            return _context.Articles.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ubtnews/ubtnews/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ubtnews.Areas.Admin.Models.Users;
+
+namespace TopNews.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        private UserManager<IdentityUser> _userManager;
+
+        public UsersController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.GetUsersInRoleAsync("NormalUser");
+
+            return View(users);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUserModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_userManager.FindByNameAsync(user.Email).Result == null)
+                {
+                    var u = new IdentityUser();
+                    u.UserName = user.Email;
+                    u.Email = user.Email;
+
+                    IdentityResult result = _userManager.CreateAsync(u, user.Password).Result;
+
+                    if (result.Succeeded)
+                    {
+                        await _userManager.AddToRoleAsync(u, "NormalUser");
+
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        foreach (var item in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, item.Description);
+                        }
+
+                        return View();
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Email already exists. Please try another one.");
+                }
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            var vm = new EditUserModel
+            {
+                Id = user.Id,
+                Email = user.Email
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditUserModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                var originalUser = await _userManager.FindByIdAsync(user.Id);
+
+                if (originalUser == null)
+                {
+                    return NotFound();
+                }
+
+                if (originalUser.Email != user.Email) // Have we changed the email?
+                {
+                    // Yes, check if unique...
+                    if (_userManager.FindByNameAsync(user.Email).Result == null)
+                    {
+                        originalUser.Email = user.Email;
+                        originalUser.UserName = user.Email;
+
+                        await _userManager.UpdateAsync(originalUser);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Email already exists. Please try another one.");
+
+                        return View();
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(user.Password)) // Are we trying to set a new password?
+                {
+                    // Yes, update it
+
+                    string code = await _userManager.GeneratePasswordResetTokenAsync(originalUser);
+                    var result = await _userManager.ResetPasswordAsync(originalUser, code, user.Password);
+
+                    if (!result.Succeeded)
+                    {
+                        foreach (var item in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, item.Description);
+                        }
+
+                        return View();
+                    }
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            await _userManager.DeleteAsync(user);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
